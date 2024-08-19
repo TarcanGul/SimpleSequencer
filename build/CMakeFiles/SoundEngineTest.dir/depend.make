607d4a4e6226a896811e2a3cad7250fa557b07c7
Tarcan Gul <nailtarcan@gmail.com>
2024-08-18 22:29:07 -0400

    fix: remove build folder

--- build/CMakeFiles/SoundEngineTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SoundEngineTest.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tarcangul/projects/simple-sequencer
-# Build directory: /Users/tarcangul/projects/simple-sequencer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SoundEngineTest]=] "/Users/tarcangul/projects/simple-sequencer/build/SoundEngineTest")
-set_tests_properties([=[SoundEngineTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/tarcangul/projects/simple-sequencer/CMakeLists.txt;77;add_test;/Users/tarcangul/projects/simple-sequencer/CMakeLists.txt;0;")
