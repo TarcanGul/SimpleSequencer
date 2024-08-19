7a652b1492d5fd3bbf535ce37205447369ea2654
Tarcan Gul <nailtarcan@gmail.com>
2024-08-18 22:22:33 -0400

    fix: remove build directory, have instructions

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tarcangul/projects/simple-sequencer/tests
-# Build directory: /Users/tarcangul/projects/simple-sequencer/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
